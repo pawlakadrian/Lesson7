dc0135b27de016ded9ca78c019b06ded0a1af17c
Adrian <adr.pawlak@gmail.com>
2021-11-29 16:04:05 +0100

    add logger

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -2,7 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
